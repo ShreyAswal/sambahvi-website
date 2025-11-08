22951d4d25d94228cbbe4bc27bb586cb58e22eb8
ShreyAswal <aswalshrey@gmail.com>
2025-11-08 17:31:24 +0530

    Base updated to '/' for images to render in hosting

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // Netlify + React Router configuration
 export default defineConfig({
   plugins: [react()],
-  base: "./", // keeps asset paths relative so they work on Netlify
+  base: "/", // ensures all image paths are absolute and load correctly
 });
